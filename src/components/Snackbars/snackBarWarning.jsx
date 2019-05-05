cc78ac969e8f80979848c67bd64bba215bc46a51
jairus <jairus.bali@gmail.com>
2019-05-05 11:20:12 -0400

    cleaned unused declared variables/imports

--- src/components/Snackbars/snackBarWarning.jsx
+++ src/components/Snackbars/snackBarWarning.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
-import Button from "@material-ui/core/Button";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import ErrorIcon from "@material-ui/icons/Error";
 import InfoIcon from "@material-ui/icons/Info";
@@ -94,10 +93,6 @@ const styles2 = theme => ({
 });
 
 class CustomizedSnackbars extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   state = {
     open: this.props.isOpen
   };
